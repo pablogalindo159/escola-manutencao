a01946825e7c26902ccedb10264645772379ce4c
Pablo Galindo <pabloeduardogalindo@gmail.com>
2026-09-20 17:18:44 +0000

    fix: remover middleware do construtor SettingsController (erro 500)

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('admin');
-    }
 
     /**
      * Página de configurações Mercado Pago
